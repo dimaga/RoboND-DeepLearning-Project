195438b71e00b4d7f7ab417800a50b3f041037ac
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2018-07-04 19:31:43 +0200

    Add code to store and restore a state

--- code/find_train_hyperparams.py
+++ code/find_train_hyperparams.py
@@ -10,7 +10,9 @@ See more details at https://github.com/scikit-optimize/scikit-optimize 
 """
 
 import numpy as np
-from skopt import gp_minimize
+from skopt import gp_minimize, load, dump
+import os.path
+
 
 def f(x):
     if x[1] != 2:
@@ -19,6 +21,20 @@ def f(x):
     return -1
 
 
-res = gp_minimize(f, [(-10.0, 10.0), [1, 2, 3, 4, 5]])
+def checkpoint_saver(res):
+    dump(res, "./checkpoint.pkl", store_objective=False)
+
+
+try:
+    prev_res = load("./checkpoint.pkl")
+except FileNotFoundError:
+    prev_res = None
+
+res = gp_minimize(
+    f,
+    [(-10.0, 10.0), [1, 2, 3, 4, 5]],
+    callback=[checkpoint_saver],
+    x0=None if prev_res is None else prev_res.x_iters,
+    y0=None if prev_res is None else prev_res.func_vals)
 
 print(res['x'])
