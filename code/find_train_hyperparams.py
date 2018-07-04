d0a894addbe896096fb7967883d69daae6a270d7
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2018-07-04 22:48:13 +0200

    Save and load checkpoints to different files

--- code/find_train_hyperparams.py
+++ code/find_train_hyperparams.py
@@ -10,6 +10,7 @@ See more details at https://github.com/scikit-optimize/scikit-optimize 
 """
 
 import pickle
+import glob
 from skopt import gp_minimize
 
 MAX_CALLS = 100
@@ -21,24 +22,31 @@ def f(x):
     return -1
 
 
-def checkpoint_saver(res):
-    """Saves intermediate parameters of hyper-parameter
-    optimization in case the script fails"""
-
-    if len(res.x_iters) >= MAX_CALLS - 10:
-        return
-
-    with open("./checkpoint.pkl", "wb") as f:
-        p = pickle.Pickler(f)
-        p.dump(res.x_iters)
-        p.dump(res.func_vals)
+class CheckpointSaver():
+    def __init__(self):
+        self.__iteration = 0
 
 
-def checkpoint_loader():
+    def do(self, res):
+        """Saves intermediate parameters of hyper-parameter
+        optimization in case the script fails"""
+
+        if len(res.x_iters) >= MAX_CALLS - 10:
+            return
+
+        with open("checkpoint{:08}.pkl".format(self.__iteration), "wb") as f:
+            p = pickle.Pickler(f)
+            p.dump(res.x_iters)
+            p.dump(res.func_vals)
+
+        self.__iteration += 1
+
+
+def checkpoint_loader(fileName):
     """Loads hyper-parameter optimization parameters to start
-    learning from previously saved state"""
+    learning from previously saved checkpoint"""
 
-    with open("./checkpoint.pkl", "rb") as f:
+    with open(fileName, "rb") as f:
         u = pickle.Unpickler(f)
         x_iters = u.load()
         func_vels = u.load()
@@ -46,9 +54,12 @@ def checkpoint_loader():
     return x_iters, func_vels
 
 
-try:
-    x0, y0 = checkpoint_loader()
-except FileNotFoundError:
+checkpoints = sorted(glob.glob("*.pkl"))
+if len(checkpoints) > 1:
+    # The last checkpoint may be broken if the process was interrupted while
+    # saving it, so load the checkpoint before the last one
+    x0, y0 = checkpoint_loader(checkpoints[-2])
+else:
     x0 = None
     y0 = None
 
@@ -59,7 +70,7 @@ if x0 is not None:
 res = gp_minimize(
     f,
     [(-10.0, 10.0), [1, 2, 3, 4, 5]],
-    callback=[checkpoint_saver],
+    callback=[CheckpointSaver().do],
     x0=x0,
     y0=y0,
     n_calls=n_calls)
