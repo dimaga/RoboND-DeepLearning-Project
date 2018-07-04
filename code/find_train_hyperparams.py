4f93c22c7bcb3a238833554a6634e079dbd23501
Dmitry Gavrilenko <dmitry.gavrilenko@icloud.com>
2018-07-04 22:15:12 +0200

    Improve checkpoint save and load

--- code/find_train_hyperparams.py
+++ code/find_train_hyperparams.py
@@ -9,10 +9,10 @@ pip install scikit-optimize
 See more details at https://github.com/scikit-optimize/scikit-optimize 
 """
 
-import numpy as np
-from skopt import gp_minimize, load, dump
-import os.path
+import pickle
+from skopt import gp_minimize
 
+MAX_CALLS = 100
 
 def f(x):
     if x[1] != 2:
@@ -22,19 +22,46 @@ def f(x):
 
 
 def checkpoint_saver(res):
-    dump(res, "./checkpoint.pkl", store_objective=False)
+    """Saves intermediate parameters of hyper-parameter
+    optimization in case the script fails"""
+
+    if len(res.x_iters) >= MAX_CALLS - 10:
+        return
+
+    with open("./checkpoint.pkl", "wb") as f:
+        p = pickle.Pickler(f)
+        p.dump(res.x_iters)
+        p.dump(res.func_vals)
+
+
+def checkpoint_loader():
+    """Loads hyper-parameter optimization parameters to start
+    learning from previously saved state"""
+
+    with open("./checkpoint.pkl", "rb") as f:
+        u = pickle.Unpickler(f)
+        x_iters = u.load()
+        func_vels = u.load()
+
+    return x_iters, func_vels
 
 
 try:
-    prev_res = load("./checkpoint.pkl")
+    x0, y0 = checkpoint_loader()
 except FileNotFoundError:
-    prev_res = None
+    x0 = None
+    y0 = None
+
+n_calls = MAX_CALLS
+if x0 is not None:
+    n_calls += -len(x0)
 
 res = gp_minimize(
     f,
     [(-10.0, 10.0), [1, 2, 3, 4, 5]],
     callback=[checkpoint_saver],
-    x0=None if prev_res is None else prev_res.x_iters,
-    y0=None if prev_res is None else prev_res.func_vals)
+    x0=x0,
+    y0=y0,
+    n_calls=n_calls)
 
 print(res['x'])
